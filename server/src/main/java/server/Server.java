ec9f3e3cc234d9572e58e1458d1aa97812d92f3d
ReinnFall <jamestock18@gmail.com>
2025-03-03 12:42:12 -0700

    login handler coded

--- server/src/main/java/server/Server.java
+++ server/src/main/java/server/Server.java
@@ -1,8 +1,12 @@
 package server;
 
+import com.google.gson.Gson;
 import spark.*;
+import service.LoginService;
+import model.UserData;
 
 public class Server {
+    private final LoginService service;
 
     public int run(int desiredPort) {
         Spark.port(desiredPort);
@@ -10,12 +14,19 @@ public class Server {
         Spark.staticFiles.location("web");
 
         // Register your endpoints and handle exceptions here.
+        Spark.post("/session", this::LoginHandler);
 
         //This line initializes the server and can be removed once you have a functioning endpoint 
         Spark.init();
 
         Spark.awaitInitialization();
         return Spark.port();
+    }
+    private Object LoginHandler(Request req, Response res){
+        var userLoginInfo = new Gson().fromJson(req.body(),UserData.class);
+        userLoginInfo = service.LoginRequest(userLoginInfo);
+
+        return new Gson().toJson(userLoginInfo);
     }
 
     public void stop() {
