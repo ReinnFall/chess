384fecd491ba920dd39518ad58a41642ae8171b0
ReinnFall <jamestock18@gmail.com>
2025-03-04 14:15:14 -0700

    wrote a succesful test for login

--- server/src/test/java/service/LoginServiceTest.java
+++ server/src/test/java/service/LoginServiceTest.java
@@ -1,35 +1,39 @@
-//package service;
-//
-//import dataaccess.*;
-//import model.AuthData;
-//import model.UserData;
-//import org.junit.jupiter.api.Assertions;
-//import org.junit.jupiter.api.BeforeEach;
-//import org.junit.jupiter.api.Test;
-//import service.LoginService;
-//
-//import java.util.Objects;
-//
-//class LoginServiceTest{
-//    private UserData test;
-//    private String expectedAuthUsername;
-//    private LoginService service;
-//
-//    @BeforeEach
-//    public void beforeEach(){
-//        test = new UserData("James","Stock",null);
-//        expectedAuthUsername = "James";
-//        UserDAO userDAO = new MemoryUserDAO();
-//        AuthDAO authDAO = new MemoryAuthDAO();
-//        service = new LoginService(userDAO,authDAO);
-//
-//    }
-//
-//    @Test
-//    public void authTokenCheck() throws DataAccessException {
-//        AuthData actual = service.loginRequest(test);
-//
-//        Assertions.assertEquals(expectedAuthUsername,actual);
-//
-//    }
-//}
+package service;
+
+import dataaccess.*;
+import model.AuthData;
+import model.UserData;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import service.LoginService;
+
+import java.util.Objects;
+
+class LoginServiceTest{
+    private UserData userInput;
+    private String expectedAuthUsername;
+    private LoginService loginService;
+    private RegisterService registerService;
+
+    @BeforeEach
+    public void beforeEach(){
+
+        userInput = new UserData("James","Stock",null);
+        expectedAuthUsername = "James";
+        UserDAO userDAO = new MemoryUserDAO();
+        AuthDAO authDAO = new MemoryAuthDAO();
+        loginService = new LoginService(userDAO,authDAO);
+        registerService = new RegisterService(userDAO,authDAO);
+
+    }
+
+    @Test
+    public void authTokenUsernameCheck() throws DataAccessException {
+        registerService.registerRequest(userInput);
+        AuthData actual = loginService.loginRequest(userInput);
+
+        Assertions.assertEquals(expectedAuthUsername,actual.username());
+
+    }
+}
