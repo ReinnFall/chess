8d602da96e5afa952edc8a9517f31eaa69b67362
ReinnFall <jamestock18@gmail.com>
2025-03-04 18:27:30 -0700

    added positive and negative test case for register

--- server/src/test/java/service/LoginServiceTest.java
+++ server/src/test/java/service/LoginServiceTest.java
@@ -12,17 +12,14 @@ import java.util.Objects;
 
 class LoginServiceTest{
     private UserData userInput;
-    private UserData badInput;
-    private String expectedAuthUsername;
+    private UserData userInput2;
     private LoginService loginService;
     private RegisterService registerService;
 
     @BeforeEach
     public void beforeEach(){
-
         userInput = new UserData("James","Stock","js@mail");
-        badInput = new UserData("James","stock","js@mail");
-        expectedAuthUsername = "James";
+        userInput2 = new UserData("Jacob","Stock","js@mail");
         UserDAO userDAO = new MemoryUserDAO();
         AuthDAO authDAO = new MemoryAuthDAO();
         loginService = new LoginService(userDAO,authDAO);
@@ -31,21 +28,35 @@ class LoginServiceTest{
     }
 
     @Test
-    public void authTokenUsernameCheck() throws DataAccessException {
+    public void loginTokenCheckUsername() throws DataAccessException {
+        String expectedAuthUsername = "James";
         registerService.registerRequest(userInput);
         AuthData actual = loginService.loginRequest(userInput);
 
         Assertions.assertEquals(expectedAuthUsername,actual.username());
     }
     @Test
-    public void passwordDontMatch() throws DataAccessException{
+    public void loginPasswordsDontMatch() throws DataAccessException{
+        UserData badInput = new UserData("James","stock","js@mail");
         registerService.registerRequest(userInput);
+
         DataAccessException ex = Assertions.assertThrows(DataAccessException.class, () ->{
             loginService.loginRequest(badInput);
         });
+    }
+    @Test
+    public void registerCheckForUniqueTokens() throws DataAccessException{
+        AuthData authPerson1 = registerService.registerRequest(userInput);
+        AuthData authPerson2 = registerService.registerRequest(userInput2);
 
+        Assertions.assertNotEquals(authPerson1.authToken(),authPerson2.authToken());
+    }
+    @Test
+    public void registerUsernameAlreadyTaken() throws DataAccessException{
+        registerService.registerRequest(userInput);
 
-
-
+        DataAccessException ex = Assertions.assertThrows(DataAccessException.class, () ->{
+            registerService.registerRequest(userInput);
+        });
     }
 }
