6510a5e03893e6ac192b720d0657d1d9104ca47d
ReinnFall <jamestock18@gmail.com>
2025-04-06 21:55:36 -0600

    added top row of chessboard

--- client/src/main/java/client/TerminalChessBoard.java
+++ client/src/main/java/client/TerminalChessBoard.java
@@ -12,8 +12,24 @@ public class TerminalChessBoard{
     public void printChessBoard(ChessGame game,ChessGame.TeamColor color){
         var out = new PrintStream(System.out, true, StandardCharsets.UTF_8);
 
-        out.print(SET_BG_COLOR_DARK_GREY);
-        out.print("a b c d e f g h");
+        char[] columns;
+        if (color == ChessGame.TeamColor.WHITE){
+            columns = new char[]{'a','b','c','d','e','f','g','h'};
+        }else{
+            columns = new char[]{'h','g','f','e','d','c','b','a'};
+        }
+
+//        out.print(SET_BG_COLOR_LIGHT_GREY);
+//        out.print("a b c d e f g h");
+//        out.print(RESET_BG_COLOR);
+//        out.println();
+
+        out.print(SET_BG_COLOR_LIGHT_GREY);
+        for (char column : columns){
+            out.print(" " + column + " ");
+        }
+        out.print(RESET_BG_COLOR);
+        out.println();
 
     }
 }
