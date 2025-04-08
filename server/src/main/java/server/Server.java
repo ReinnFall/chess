238f78e55b6e616b7217d898aa0d7c1fd061319f
ReinnFall <jamestock18@gmail.com>
2025-04-08 11:07:19 -0600

    fixed broken register

--- server/src/main/java/server/Server.java
+++ server/src/main/java/server/Server.java
@@ -83,18 +83,13 @@ public class Server {
             return res.body();
     }
     private Object registerHandler(Request req, Response res) throws DataAccessException {
-        try{
-            UserData userRegisterInfo = new Gson().fromJson(req.body(),UserData.class);
-            AuthData registerResult = registerService.registerRequest(userRegisterInfo);
+        UserData userRegisterInfo = new Gson().fromJson(req.body(),UserData.class);
+        AuthData registerResult = registerService.registerRequest(userRegisterInfo);
 
-            //res.status(200);
-            res.body(new Gson().toJson(registerResult));
+        //res.status(200);
+        res.body(new Gson().toJson(registerResult));
 
-            return res.body();
-        } catch (DataAccessException ex){
-            res.status(ex.statusCode());
-            return new Gson().toJson(ex.getMessage());
-        }
+        return res.body();
     }
     private Object logoutHandler(Request req, Response res) throws DataAccessException, SQLException {
         String authToken = req.headers("authorization");
